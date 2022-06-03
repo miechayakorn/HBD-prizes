543d46a29e1613d4ba8e747ae40ed23b940c9868
Mie Chayakorn <miechayakorn@gmail.com>
2022-06-03 13:34:46 +0700

    [Add] No of row in Leaderboard

--- pages/games/index.js
+++ pages/games/index.js
@@ -64,27 +64,33 @@ const Games = () => {
                         }}
                     >
                         <Table.Header>
+                            <Table.Column>No</Table.Column>
                             <Table.Column>NAME</Table.Column>
                             <Table.Column>Score</Table.Column>
                         </Table.Header>
                         <Table.Body>
                             <Table.Row key="1">
+                                <Table.Cell>1</Table.Cell>
                                 <Table.Cell>Tony Reichert</Table.Cell>
                                 <Table.Cell>Active</Table.Cell>
                             </Table.Row>
                             <Table.Row key="2">
+                                <Table.Cell>2</Table.Cell>
                                 <Table.Cell>Zoey Lang</Table.Cell>
                                 <Table.Cell>Paused</Table.Cell>
                             </Table.Row>
                             <Table.Row key="3">
+                                <Table.Cell>3</Table.Cell>
                                 <Table.Cell>Jane Fisher</Table.Cell>
                                 <Table.Cell>Active</Table.Cell>
                             </Table.Row>
                             <Table.Row key="4">
+                                <Table.Cell>4</Table.Cell>
                                 <Table.Cell>William Howard</Table.Cell>
                                 <Table.Cell>Vacation</Table.Cell>
                             </Table.Row>
                             <Table.Row key="5">
+                                <Table.Cell>5</Table.Cell>
                                 <Table.Cell>William Howard</Table.Cell>
                                 <Table.Cell>Vacation</Table.Cell>
                             </Table.Row>
