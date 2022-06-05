74b70e401874cac6ea4f745e23cffd8cde688782
Mie Chayakorn <miechayakorn@gmail.com>
2022-06-05 21:20:22 +0700

    Hotfix/Link Register

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Button, Container, Grid, Loading, Row, Text } from '@nextui-org/react'
 import { InstagramIcon } from '../components/InstagramIcon'
 import ModalCheckAccount from '../components/ModalCheckAccount'
+import Router from 'next/router'
 
 const Login = () => {
     const [isLoadingBtn, setIsLoadingBtn] = useState(false)
@@ -16,13 +17,15 @@ const Login = () => {
             <Container className="App bg-ig-dot" style={{minHeight}}>
                 <Grid.Container gap={2}>
                     <Grid xs={12} justify="center">
-                        <Text h1 size={40} css={{
-                            textGradient: '45deg, $yellow600 -20%, $red600 100%',
-                            mb: '5px'
+                        <Text h1 size={50} css={{
+                            textGradient: '45deg, $yellow600 -20%, $red600 100%'
                         }}>
                             Login
                         </Text>
                     </Grid>
+                    <Row justify="center" css={{mb: '20px'}}>
+                        <Button color={'gradient'} size="sm" onClick={() => Router.push('/register')}>Register</Button>
+                    </Row>
                     <Row>
                         <Text>** ต้องทำการเพิ่มแอป Mie Arcade ก่อนกด Login ตรวจสอบสถานะคำเชิญ
                             <Button size="xs" onClick={() => setIsModal(true)}>โดยกดที่นี่</Button>
