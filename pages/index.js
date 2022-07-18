8c0714bb42bc93e19bbcf36e2c745a014d45ebf6
Mie Chayakorn <miechayakorn@gmail.com>
2022-07-18 19:26:25 +0700

    Hotfix/loading-spline

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '@nextui-org/react'
 import NextLink from 'next/link'
 
 const Home = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    const interval = setInterval(() => {
+        console.log('tet')
+        setIsLoading(false)
+        clearInterval(interval)
+    }, 5000)
 
     return <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}}>
         <iframe src="https://my.spline.design/untitled-3bb2f3f5166fc6ce53f356bf3a0eda92" width="100%" height="100%"/>
         <div style={{position: 'fixed', bottom: '40px', left: '50%'}}>
             <div style={{position: 'relative', left: '-50%'}}>
-                <NextLink href={'/games'}>
-                    <Button className="blob" rounded flat>Get Start!</Button>
-                </NextLink>
+                {isLoading ? <Button disabled rounded flat>Loading...</Button> :
+                    <NextLink href={'/games'}>
+                        <Button className="blob" rounded flat>Get Start!</Button>
+                    </NextLink>}
             </div>
         </div>
     </div>
